9110bb7cfc713fc71936e23adb2172e23176d448
Eric Fluharty <fluinc@gmail.com>
2026-03-08 21:10:26 -0400

    psr 17 middlware interface

--- src/Interfaces/MiddlewareInterface.php
+++ src/Interfaces/MiddlewareInterface.php
@@ -2,18 +2,6 @@
 
 namespace APIRouter\Interfaces;
 
-use APIRouter\Http\Request;
-use APIRouter\Http\Response;
-
-interface MiddlewareInterface
+interface MiddlewareInterface extends \Psr\Http\Server\MiddlewareInterface
 {
-    /**
-     * Invoke the middleware
-     *
-     * @param Request $request
-     * @param Response $response
-     * @param callable $next Next middleware or handler in the chain
-     */
-    
-    public function __invoke(Request $request, Response $response, callable $next);
 }
